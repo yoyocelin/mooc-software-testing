bb37467003958deb9db5952bb4c9f7b33e9ae45e
Yoelin <yoselin@rosario.com>
2021-03-11 22:22:12 -0400

    Agregando metodo beforeeach a roman numeral test

--- src/test/java/tudelft/roman/RomanNumeralTest.java
+++ src/test/java/tudelft/roman/RomanNumeralTest.java
@@ -1,10 +1,17 @@
 package tudelft.roman;
 
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class RomanNumeralTest {
 
+    private RomanNumeral roman;
+
+    @BeforeEach
+    public void initialize(){
+        this.roman = new RomanNumeral();
+    }
 
     @Test
     public void singleNumber() {
@@ -15,21 +22,18 @@ public class RomanNumeralTest {
 
     @Test
     public void numberWithManyDigits() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("VIII");
         Assertions.assertEquals(8, result);
     }
 
     @Test
     public void numberWithSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("IV");
         Assertions.assertEquals(4, result);
     }
 
     @Test
     public void numberWithAndWithoutSubtractiveNotation() {
-        RomanNumeral roman = new RomanNumeral();
         int result = roman.convert("XLIV");
         Assertions.assertEquals(44, result);
     }
